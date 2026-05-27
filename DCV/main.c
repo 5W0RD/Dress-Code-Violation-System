3d1bce4931edecd216bd15c9936dbc3ce890467b
5W0RD <batoydavidjosh@gmail.com>
2026-05-28 01:02:06 +0800

    Update main.c

--- DCV/main.c
+++ DCV/main.c
@@ -7,15 +7,25 @@ int main() {
 
     do {
 
-        printf("\n===== UNIFORM / DRESS CODE VIOLATION LOG =====\n");
+        printf("\n===== DRESS CODE VIOLATION SYSTEM =====\n");
 
         printf("1. Record Violation\n");
         printf("2. View Violations\n");
         printf("3. Search Student\n");
-        printf("0. Exit\n");
+        printf("4. Edit Student\n");
+        printf("5. Clean Record\n");
+        printf("6. Exit\n");
 
         printf("\nEnter Choice: ");
-        scanf("%d", &choice);
+
+        while(scanf("%d", &choice) != 1) {
+
+            printf("Invalid input! Numbers only.\n");
+
+            while(getchar() != '\n');
+
+            printf("Enter Choice: ");
+        }
 
         switch(choice) {
 
@@ -31,15 +41,23 @@ int main() {
                 searchStudent();
                 break;
 
-            case 0:
-                printf("\nExiting Program...\n");
+            case 4:
+                editStudent();
+                break;
+
+            case 5:
+                cleanRecord();
+                break;
+
+            case 6:
+                printf("Exiting Program...\n");
                 break;
 
             default:
-                printf("\nInvalid Choice!\n");
+                printf("Invalid Choice!\n");
         }
 
-    } while(choice != 0);
+    } while(choice != 6);
 
     return 0;
 }
